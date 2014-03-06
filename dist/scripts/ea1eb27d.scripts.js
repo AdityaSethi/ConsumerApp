b2e304b9a9e7970aa0e7f9ef28bdcb92683e7709
Aditya Sethi <aditya_sethi@external.mckinsey.com>
2014-03-06 15:03:52 +0530

    dist folder files pushed, reason being bootstrap not working

---
+++ dist/scripts/ea1eb27d.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("consumerAppApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("consumerAppApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
